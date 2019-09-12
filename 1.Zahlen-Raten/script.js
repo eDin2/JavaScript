f4edabb974c545c0963a5ff895ef43ed9fa5f02b
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-12 22:55:24 +0200

    Name des Spiels

--- 1.Zahlen-Raten/script.js
+++ 1.Zahlen-Raten/script.js
@@ -1,11 +1,22 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
     // JS wird erst nach dem laden der Seite ausgeführt!
-    
-    
+
+
     /* *********************** */
     /* Zufallszahlen erzeugen */
+    /* math.random (Helferfunktion) */
     /* *********************** */
-    document.querySelector
+    document.querySelector('#submit').addEventListener("click", ausgabeZufallszahl);
+    let randomNumberMin = 1;
+    let randomNumberMax = 10;
+
+    function rand(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    function ausgabeZufallszahl() {
+        document.querySelector('output').innerText = rand(randomNumberMin, randomNumberMax);
+    }
 
 });
