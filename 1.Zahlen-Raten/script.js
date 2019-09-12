7b5bed289ba13238e8248c015d977aac0a832878
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-12 22:33:47 +0200

    x

---
+++ 1.Zahlen-Raten/script.js
@@ -0,0 +1,11 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    // JS wird erst nach dem laden der Seite ausgeführt!
+    
+    
+    /* *********************** */
+    /* Zufallszahlen erzeugen */
+    /* *********************** */
+    document.querySelector
+
+});
