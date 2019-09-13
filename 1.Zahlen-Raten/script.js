6e9585bf2bc4a215c082988ee0ce4d177e4edaf0
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-14 00:30:49 +0200

    Beauty

--- 1.Zahlen-Raten/script.js
+++ 1.Zahlen-Raten/script.js
@@ -1,22 +1,39 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function () {
-    // JS wird erst nach dem laden der Seite ausgeführt!
+document.addEventListener("DOMContentLoaded", function() {
+  // JS wird erst nach dem laden der Seite ausgeführt!
 
 
-    /* *********************** */
-    /* Zufallszahlen erzeugen */
-    /* math.random (Helferfunktion) */
-    /* *********************** */
-    document.querySelector('#submit').addEventListener("click", ausgabeZufallszahl);
-    let randomNumberMin = 1;
-    let randomNumberMax = 10;
+  /* *********************** */
+  /* Zufallszahlen erzeugen */
+  /* math.random (Helferfunktion) */
+  /* *********************** */
+  document.querySelector('#submit').addEventListener("click", checkInput);
+  // An den submit-Button wird mit addEventListener ein EventListener angehängt,
+  // der beim Auslösen des Klickereignisses die Funktion checkInput() aufruft.
 
-    function rand(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min)
+  let input = document.getElementById('guessField');
+  let output = document.querySelector('output');
+  let randomNumberMin = 1;
+  let randomNumberMax = 10;
+  let randomNumber = rand(randomNumberMin, randomNumberMax);
+  let guess = 1;
+
+  function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  function checkInput() {
+    console.log('Zufallszahl: ' + randomNumber + '   input: ' + input.value + '  Versuche(guess): ' + guess);
+    if (input.value == randomNumber) {
+      output.innerText = 'Glückwunsch! Sie haben es in  ' + guess + ' Versuch(en) erraten!';
+    } else if (input.value > randomNumber) {
+      guess++;
+      output.innerText = 'Schade! Versuchen Sie eine kleinere Zahl.';
+    } else {
+      guess++;
+      output.innerText = 'Schade! Versuchen Sie eine größere Zahl.';
     }
-
-    function ausgabeZufallszahl() {
-        document.querySelector('output').innerText = rand(randomNumberMin, randomNumberMax);
-    }
+    input.value = '';
+  }
 
 });
