042549f0cd793f1819ecdbfb3db375a64f977561
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-17 22:30:36 +0200

    Dice Game randomImgSource erweitert

--- 3.Dice_Game/index.js
+++ 3.Dice_Game/index.js
@@ -1,3 +1,7 @@
 "use strict";
 
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6) + 1; // Nummer 1-6
+
+let randomDiceImg = "dice" + randomNumber1 + "png"; // dice1.png - dice6.png
+
+let randomImgSource = "images/" + randomDiceImg; //images/dice1.png - images/dice6.png
