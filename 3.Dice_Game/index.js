7f6ae0b5f7e090b57fa0f586d943bd158754a121
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-18 20:55:50 +0200

    Zweiten Wuerfel erstellt

--- 3.Dice_Game/index.js
+++ 3.Dice_Game/index.js
@@ -1,10 +1,20 @@
 "use strict";
 
+/* Erster Würfell! */
 let randomNumber1 = Math.floor(Math.random() * 6) + 1; // Nummer 1-6
-
 let randomDiceImg = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
-
 let randomImgSource = "images/" + randomDiceImg; //images/dice1.png - images/dice6.png
-
 let image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImgSource);
+
+
+
+
+/* Zweiter Würfell! */
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomImgSource2 = "images/dice" + randomNumber2 + ".png";
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImgSource2);
+
+
+
