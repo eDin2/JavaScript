5c9817f2e72f36eac08a761302362ba9706d4327
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-18 06:33:58 +0200

    random img

--- 3.Dice_Game/index.js
+++ 3.Dice_Game/index.js
@@ -2,6 +2,9 @@
 
 let randomNumber1 = Math.floor(Math.random() * 6) + 1; // Nummer 1-6
 
-let randomDiceImg = "dice" + randomNumber1 + "png"; // dice1.png - dice6.png
+let randomDiceImg = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
 
 let randomImgSource = "images/" + randomDiceImg; //images/dice1.png - images/dice6.png
+
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImgSource);
