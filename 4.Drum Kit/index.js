30ee6cd3b5e8aa3726df5138d7f00168b50420ed
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-19 13:02:41 +0200

    playSounds hinzugefüght muss die btns dazu geben welcher btn wurd gecklickt

--- 4.Drum Kit/index.js
+++ 4.Drum Kit/index.js
@@ -1,10 +1,20 @@
 "use strict";
 
 
-/* Hiermit werden alle Buttons itteriert und damit auch asnprechbar gemacht */
+/* Hiermit werden alle Buttons iteriert und damit auch asnprechbar gemacht */
 let numberOfButtons = document.querySelectorAll(".drum").length;
+
 for (let i = 0; i < numberOfButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("Ich wurde geklickt");
-    });
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    // eine Anonyme funktion wird aufgerufen die folgenden code ausfuert
+
+    // Welcher button wurde gedrückt und welcher sound muss ausgeführt werden?
+    let audioPlay = new Audio('sounds/crash.mp3');
+    audioPlay.play(this);
+
+  });
 }
+
+/* Play Sound */
+// let audioPlay = new Audio('sounds/crash.mp3');
+// audioPlay.play();
