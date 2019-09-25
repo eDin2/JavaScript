5383a4dbb4a162360ddf4a43272e1b10561dcca8
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-09-25 20:33:06 +0200

    anomatePress erstellt

--- 6.SimonGame/answer.js
+++ 6.SimonGame/answer.js
@@ -27,6 +27,22 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
+function animatePress(currentColor) {
+
+  //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+  $("#" + currentColor).addClass("pressed");
+
+  //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
