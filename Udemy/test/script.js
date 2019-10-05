3ba841d75afa754d9abe35d1c8c91e5b438c1d1a
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-10-05 13:08:26 +0200

    test

---
+++ Udemy/test/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+window.addEventListener("load", (blubb, blubber) => {
+  let machWas = () => {
+    if ("undifined" != blubb) {
+      alert("blubb");
+    }
+    if ("undifined" != blubber) {
+        alert("blubber");
+    }
+    machWas("laber Nix!");
+  };
+});
