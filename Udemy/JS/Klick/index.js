769f1ad6c2775f81523ef9550ef2cb13305bd201
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-10-05 13:07:36 +0200

    klick fertig

--- Udemy/JS/Klick/index.js
+++ Udemy/JS/Klick/index.js
@@ -1,11 +1,22 @@
 "use strict";
 
 window.addEventListener("load", function() {
-  let button = document.getElementById("btn"); // 1.der button soll ausgewählt werden
+  /* Click on btn1 */
+  let button1 = document.getElementById("btn1"); // 1.der button1 soll ausgewählt werden
   let count = 0; // 2.Zähler wird definiert
-  button.onclick = function() {
-    // 3.auf den button soll mittels click zugegriffen werden
+
+  button1.onclick = () => {
+    // 3.auf den button1 soll mittels click zugegriffen werden
     count += 1; // 4.bei jedem Click soll der Zähler um eines dazu gerechnet werden
-    button.innerText = "Count: " + count; // 5.gib den Zähler mittels innerText im bitton aus!
+    button1.innerText = "Count: " + count; // 5.gib den Zähler mittels innerText im bitton aus!
+  };
+
+  /* Click on btn2 */
+  let button2 = document.getElementById("btn2");
+  let count2 = 0;
+
+  let fun = () => {
+    count2 += 1;
+    console.log(fun);
   };
 });
