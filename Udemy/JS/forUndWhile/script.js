955f11f900f0b0da5333dc6c597d9b2f7ebb51a1
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-10-05 22:53:32 +0200

    for and while schleifen

---
+++ Udemy/JS/forUndWhile/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+window.addEventListener("load", () => {
+
+  let array = [
+    "Saban",
+    "Edo",
+    "Marija",
+    "Hesus"
+  ]
+
+  for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+
+
+
+  let a = 0;
+  while (a < 10) { // solange a kleiner 10 ist, immer eines dazu adieren
+    a++;
+    console.log(a);
+  }
+
+
+});
