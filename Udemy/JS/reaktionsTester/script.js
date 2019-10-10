7363c70ba8ea2138524274194d6df64c3ce36b73
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-10-10 23:03:57 +0200

    Form verändert nun Ihren radius, und aus einem qube wird ein kreis

--- Udemy/JS/reaktionsTester/script.js
+++ Udemy/JS/reaktionsTester/script.js
@@ -3,21 +3,33 @@ window.addEventListener("load", function() {
   let start = new Date().getTime();
 
   /* Funktion die eine zufällige farbe erstellt */
+  /* es wird ein zufaälliger HEX wert erstellt */
   /* ############################################################# */
-  function getRandomColor() {
+  let getRandomColor = () => {
     var letters = "0123456789ABCDEF";
     var color = "#";
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+  };
 
   /* ############################################################# */
   let makeShapeAppear = () => {
     let top = Math.random() * 300;
     let left = Math.random() * 500;
     let width = Math.random() * 300;
+
+    /* mit borderRadius werden die ecken um 50% gerundet und daher */
+    /* ergibt das alle am ende eine zufälligen Kreis! */
+    /* ########################################################## */
+    if (Math.random() < 0.5) {
+      document.getElementById("form").style.borderRadius = "50%";
+    } else {
+      document.getElementById("form").style.borderRadius = 0;
+    }
+
+    document.getElementById("form").style.backgroundColor = getRandomColor();
     document.getElementById("form").style.top = top + "px";
     document.getElementById("form").style.left = left + "px";
     document.getElementById("form").style.width = width + "px";
