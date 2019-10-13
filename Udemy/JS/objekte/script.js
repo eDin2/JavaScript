73a2f04c0f4377d4c002d4a70d97916d52964f5d
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-10-13 09:25:11 +0200

    ordner für objekte angelegt

---
+++ Udemy/JS/objekte/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+window.addEventListener("load", function() {
+    let person = {
+        firstName:"Saban",
+        age: 21
+    }
+
+    let attr = "firstName";
+    console.log(person[attr]);
+    
+});
