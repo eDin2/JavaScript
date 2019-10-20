8c77fd85430965c674eed1a9ef93675395f80729
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-10-20 21:00:52 +0200

    verschönerne den code

--- Udemy/JS/bankKonto/script.js
+++ Udemy/JS/bankKonto/script.js
@@ -1,27 +1,27 @@
 "use strict";
 
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
 
-    class bankKonto {
+  class bankKonto {
 
-        constructor(guthaben) {
-            this.guthaben = guthaben;
-        }
-
-        abheben(menge) {
-            let newGuthaben = this.guthaben - menge;
-            if (newGuthaben < 0) {
-                alert("Oida in minus kannst du aber nicht!");
-            } else {
-                this.guthaben = this.guthaben - menge;
-            }
-        }
+    constructor(guthaben) {
+      this.guthaben = guthaben;
     }
 
-    let b = new bankKonto(1000);
-    b.abheben(200);
-    b.abheben(900);
-    console.log(b);
+    abheben(menge) {
+      let newGuthaben = this.guthaben - menge;
+      if (newGuthaben < 0) {
+        alert("Oida in minus kannst du aber nicht!");
+      } else {
+        this.guthaben = this.guthaben - menge;
+      }
+    }
+  }
+
+  let b = new bankKonto(1000);
+  b.abheben(200);
+  b.abheben(900);
+  console.log(b);
 
 
 });
