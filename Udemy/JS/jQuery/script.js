e0317d42e5816aeaa79e08691523b1ce06b214bb
eDin2 <37242498+eDin2@users.noreply.github.com>
2019-11-01 23:17:29 +0100

    schleifen

--- Udemy/JS/jQuery/script.js
+++ Udemy/JS/jQuery/script.js
@@ -1,12 +1,32 @@
 "use stict";
 
-$(document).ready(function() {
-  let p = $("p.absatz");
+$(document).ready(function () {
+    /*   let p = $("p.absatz");
+      p.css("color", "red");
+      $("p").css("color", "green");
+     */
 
-  p.css("color", "red");
-  // $("p").css("color", "green");
+    /* Erreignisse */
+    /* ############ */
+    $("h2")
+        .click(function () {
+            $(this).css("color", "red");
+        })
+        .mouseover(function () {
+            $(this).css('color', 'blue');
+        })
+        .mouseleave(function () {
+            $(this).css('color', 'black');
+        })
 
-  $("h2").click(function() {
-    $(this).css("color", "red");
-  });
+
+    /* Schleifen */
+    /* ############ */
+    $('h2').each(function (i, element) {
+        if (i % 2 == 0) {
+            $(this).css('color', 'green');
+        } else {
+            $(this).css('color', 'yellow')
+        }
+    });
 });
