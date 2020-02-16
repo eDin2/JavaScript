d4737d38f3d8986bd818ce7bf65dd33e4e9e7cda
eDin2 <37242498+eDin2@users.noreply.github.com>
2020-02-16 12:18:23 +0100

    server.js erstellt

---
+++ 8.Express_Server/server.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello Junge');
+});
+
+app.get('/contact', function () {
+    res.send("Ruf mich AN!");
+});
+
+
+app.get('/about', function () {
+    res.send("Mein Name ist ????!");;
+});
+
+app.listen(3000, function () {
+    console.log("Server leuft auf Port: 3000");
+});
