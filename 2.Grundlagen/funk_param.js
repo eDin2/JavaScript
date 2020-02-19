1e694986f7983c78adbfa5fd67e7816209ba9f82
eDin2 <37242498+eDin2@users.noreply.github.com>
2020-02-19 22:52:01 +0100

    funktionen

---
+++ 2.Grundlagen/funk_param.js
@@ -0,0 +1,11 @@
+//jshint esversion: 6
+
+"use strict";
+const cost = 1.5; 
+let getMilk = function (mony) {
+    let calculate = Math.floor(mony/cost);
+    console.log(calculate);
+    
+};
+getMilk(5);
+
